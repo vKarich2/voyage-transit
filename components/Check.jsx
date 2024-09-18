18f064a9301f591c7acd4d5ad291887ef895b0e5
vKarich2 <vladyslav.karapetov@nure.ua>
2024-09-18 21:19:19 +0200

    fix: little fix services page

--- components/Check.jsx
+++ components/Check.jsx
@@ -4,7 +4,7 @@ export default function Check({ title }) {
   return (
     <div className="flex gap-x-2 items-center">
       <CheckIcon className="w-10 h-10 text-accent" />
-      <h3 className="text-[20px] uppercase font-semibold text-foreground">
+      <h3 className="text-xl uppercase font-semibold text-foreground">
         {title}
       </h3>
     </div>
